51633b9a24dc671b73e79b4e547e5bd787031613
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-10-25 14:05:57 +0200

    Dodaj pliki projektów.

---
+++ 4M07mobROT13/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _4M07mobROT13
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ 4M07mobROT13/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace _4M07mobROT13
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void btnSzyfruj(object sender, EventArgs e)
+        {
+            string s = entJawny.Text.ToLower();
+            string w = "";
+            string litery = "abcdefghijklmnopqrstuvwxyz";
+            foreach(var c in s)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    int poz = (litery.IndexOf(c) + 13) % 26;
+                    w += litery[poz];
+                }
+            }
+                
+
+            /*
+            char znak;
+            foreach(var c in s)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    znak = (char)(c + 13);
+                    if (znak > 'z')
+                        znak = (char)(znak - 26);
+                    w += znak;
+                }
+                else
+                    w += c;
+            }
+            */
+            entZaszyfr.Text = w;
+
+        }
+
+
+    }
+
+}
